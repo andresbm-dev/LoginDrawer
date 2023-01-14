1266c766f4ae41ad062dd55eef313ff4322ef866
Andresbm_10 <andresbm_10@hotmail.com>
2023-01-14 14:44:53 -0500

    feat: add auth phone firebase login

--- app/src/main/java/com/abm/testdashfleet/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/abm/testdashfleet/ui/activities/LoginActivity.kt
@@ -3,17 +3,132 @@ package com.abm.testdashfleet.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import cn.pedant.SweetAlert.SweetAlertDialog
 import com.abm.testdashfleet.databinding.ActivityLoginBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import java.util.concurrent.TimeUnit
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding:ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
+    private var storedVerificationId: String? = ""
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                Log.d(TAG, "onVerificationCompleted:$credential")
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                Log.w(TAG, "onVerificationFailed", e)
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e is FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                }
+                showAlertError("error de identificaciòn")
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:$verificationId")
+
+                // Save verification ID and resending token so we can use them later
+                storedVerificationId = verificationId
+                resendToken = token
+            }
+        }
+        // [END phone_auth_callbacks]
 
         binding.login.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            validateCompleteFields()
         }
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = task.result?.user
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                    // Update UI
+                }
+            }
+    }
+    private fun verifyPhoneNumberWithCode(verificationId: String?, code: String) {
+        // [START verify_with_code]
+        val credential = PhoneAuthProvider.getCredential(verificationId!!, code)
+        // [END verify_with_code]
+    }
+
+    private fun validateCompleteFields() {
+        val cellphone = binding.fieldCellphone.text.toString()
+        val password = binding.editTextNumberPassword.text.toString()
+        if (cellphone.isEmpty() || password.isEmpty()) {
+            showAlertError("Campos Incompletos \n Favor llenar TODOS los campos ")
+        } else {
+            if (cellphone.length < 13){
+                showAlertError("Faltan digitos del celular")
+            }else{
+                requestAuthFirebase()
+
+            }
+        }
+
+
+    }
+
+    private fun requestAuthFirebase() {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(binding.fieldCellphone.text.toString())       // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)                 // Activity (for callback binding)
+            .setCallbacks(callbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private fun showAlertError(message : String) {
+        SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+            .setTitleText("Ooops..")
+            .setContentText(message)
+            .show()
+    }
+
+    companion object {
+        private const val TAG = "PhoneAuthActivity"
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
