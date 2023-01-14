4ba3cb0b1b9d2f92375416d73d89bc08dd08b5c9
Andresbm_10 <andresbm_10@hotmail.com>
2023-01-14 09:34:33 -0500

    feat: add drawer and bottom navigation

--- app/src/main/java/com/abm/testdashfleet/MainActivity.kt
+++ app/src/main/java/com/abm/testdashfleet/MainActivity.kt
@@ -2,10 +2,93 @@ package com.abm.testdashfleet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.abm.testdashfleet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.main_nav_host)
+
+        appBarConfiguration = AppBarConfiguration.Builder(R.id.homeFragment, R.id.additional_fragment,
+            R.id.credit_card_fragment, )
+            .setOpenableLayout(binding.mainDrawerLayout)
+            .build()
+
+        setSupportActionBar(binding.mainToolbar)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        visibilityNavElements(navController)
+
+    }
+
+    private fun visibilityNavElements(navController: NavController) {
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.additional_fragment -> showBothNavigation()
+                R.id.adjust_fragment -> showBothNavigation()
+                else -> showBothNavigation()
+            }
+        }
+
+    }
+
+    private fun hideBothNavigation() { //Hide both drawer and bottom navigation bar
+        binding.mainBottomNavigationView.visibility = View.GONE
+        binding.mainNavigationView.visibility = View.GONE
+        binding.mainDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED) //To lock navigation drawer so that it don't respond to swipe gesture
+    }
+
+    private fun hideBottomNavigation() { //Hide bottom navigation
+        binding.mainBottomNavigationView.visibility = View.GONE
+        binding.mainNavigationView.visibility = View.VISIBLE
+        binding.mainDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED) //To unlock navigation drawer
+
+        binding.mainNavigationView.setupWithNavController(navController) //Setup Drawer navigation with navController
+    }
+
+    private fun showBothNavigation() {
+        binding.mainBottomNavigationView.visibility = View.VISIBLE
+        binding.mainNavigationView.visibility = View.VISIBLE
+        binding.mainDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+        setupNavControl()
+    }
+
+    private fun setupNavControl() {
+        binding.mainNavigationView.setupWithNavController(navController)
+        binding.mainBottomNavigationView.setupWithNavController(navController)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
+
+    override fun onBackPressed() {
+        when { //If drawer layout is open close that on back pressed
+            binding.mainDrawerLayout.isDrawerOpen(GravityCompat.START) -> {
+                binding.mainDrawerLayout.closeDrawer(GravityCompat.START)
+            }
+            else -> {
+                super.onBackPressed() //If drawer is already in closed condition then go back
+            }
+        }
     }
 }
